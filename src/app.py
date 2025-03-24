4e51de417bde655a51b64b12eca90a7cf4930800
GISELLEQASCM <gipachecof@gmail.com>
2025-03-24 16:20:49 +0000

    Remove espaços em branco desnecessários no código

--- src/app.py
+++ src/app.py
@@ -103,3 +103,5 @@ def signup_for_activity(activity_name: str, email: str):
         raise HTTPException(status_code=400, detail="Student is already signed up for this activity")
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
+    
+    
